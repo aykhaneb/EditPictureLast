94bd73ae1737c3c28c39358229a764e653a8a89d
user <user@WIN-GOUM6HBH1LC>
2023-07-29 14:32:11 +0400

    Add project files.

---
+++ EditPicture/PictureManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditPictureTask
+{
+    internal class PictureManager
+    {
+        internal Bitmap ColorFormatting(Bitmap image, int red, int green, int blue)
+        {
+            int r, g, b;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color pixelColor = image.GetPixel(i, j);
+
+                    r = pixelColor.R + red;
+                    r = r % 255;
+                    g = pixelColor.G + green;
+                    g = g % 255;
+                    b = pixelColor.B + blue;
+                    b = b % 255;
+
+                    Color newColor = Color.FromArgb(r, g, b);
+
+                    image.SetPixel(i, j, newColor);
+                }
+            }
+
+            return image;
+        }
+
+        internal void SaveFile(Bitmap image)
+        {
+            Console.Write("Enter the file name:");
+            string newPicture = Console.ReadLine() + ".jpeg";
+
+            image.Save(newPicture);
+
+            Console.WriteLine("Image saved");
+        }
+    }
+}
---
+++ EditPicture/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+
+namespace EditPictureTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EditManager EditManager = new EditManager();
+
+            string apply = "";
+
+            while (apply != "end")
+            {
+                Console.Write("Enter Image location:");
+                string imageLocation = Console.ReadLine();
+
+                Bitmap image = new Bitmap(imageLocation);
+
+                Console.WriteLine($"1.Random Filter\n2.Custom Filter");
+
+            start:
+
+                apply = Console.ReadLine();
+
+                if (apply.Equals("1"))
+                {
+                    var random = new Random();
+                    int r = random.Next(0, 255);
+                    int g = random.Next(0, 255);
+                    int b = random.Next(0, 255);
+
+                    Bitmap newRandomPicture = EditManager.ColorFormatting(image, r, g, b);
+                    EditManager.SaveFile(newRandomPicture);
+                    image.Dispose();
+                }
+
+                else if (apply.Equals("2"))
+                {
+                    Console.Write("Red:");
+                    int red = int.Parse(Console.ReadLine());
+                    Console.Write("Green:");
+                    int green = int.Parse(Console.ReadLine());
+                    Console.Write("Blue:");
+                    int blue = int.Parse(Console.ReadLine());
+
+                    Bitmap newPicture = EditManager.ColorFormatting(image, red, green, blue);
+
+                    EditManager.SaveFile(newPicture);
+
+                    image.Dispose();
+                }
+
+                else
+                {
+
+                    goto start;
+                }
+            }
+        }
+    }
+}
